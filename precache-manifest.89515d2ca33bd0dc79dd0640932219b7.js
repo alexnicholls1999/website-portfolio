8c2bb90c525c54c127d870970621a0bbc73430e8
alexnicholls1999 <azini@live.co.uk>
2020-07-22 11:17:09 +0100

    Updates

---
+++ precache-manifest.89515d2ca33bd0dc79dd0640932219b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48e4e847016378b489aa2411f81fa76",
+    "url": "/website-portfolio/index.html"
+  },
+  {
+    "revision": "e8572810d0c81a804e57",
+    "url": "/website-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d3d8982466512b8aef6",
+    "url": "/website-portfolio/static/js/2.702f3ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website-portfolio/static/js/2.702f3ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8572810d0c81a804e57",
+    "url": "/website-portfolio/static/js/main.67db9c1e.chunk.js"
+  },
+  {
+    "revision": "201b6b604a809554bce5",
+    "url": "/website-portfolio/static/js/runtime-main.ed5f0b39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
