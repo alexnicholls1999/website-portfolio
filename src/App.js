b2a5e72ce34ff6edf21af2b14a7c8ab01de7f6a3
alexnicholls1999 <azini@live.co.uk>
2020-09-08 08:38:07 +0100

    add navbar changes

--- src/App.js
+++ src/App.js
@@ -1,122 +1,28 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {useIntersection, useGetSetState} from "react-use";
-import gsap, {TweenLite, Power4} from "gsap";
+import React from 'react';
+import theme from "./config/theme.js";
 import GlobalStyles from "./config/GlobalStyles";
-import styled, {ThemeProvider} from "styled-components";
-import theme from "./config/theme";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./Components/Header";
-import Tile from "./Components/Tile";
-import { Container , Col, Row } from "react-bootstrap";
-import { intersectionTypeAnnotation } from '@babel/types';
-import Footer from "./Components/Footer";
+import {ThemeProvider} from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
 
+// Components 
 
-// Tile Images 
-import ProjectA from "./assets/Projects/Project_A.png";
-import ProjectB from "./assets/Projects/Project_B.png";
-import ProjectC from "./assets/Projects/Project_C.gif";
-import ProjectD from "./assets/Projects/Project_D.gif";
+import Work from "./Views/Work";
+import About from "./Views/About";
 
-const HeaderBorder = styled.div`
-  padding: 15%;
-  margin-top: -1%;
-  background-color: white;
-  `;
-
-const Projects = styled.main`
-  padding-bottom: 10%;
-  background-color: white;
-  
-`;
-
-
-function App (){
-
-  const sectionRef = useRef(null);
-  const tileRef = useRef(null);
-
-  const [hover, setHover] = useState(false);
-
-  const intersection = useIntersection(sectionRef, {
-    root: null,
-    rootMargin: '1500px',
-    threshold: 1
-  });
-
-  const fadeIn = element => {
-    gsap.to(element, 1, {
-      opacity: 1,
-      y: -60,
-      ease: "power4.out",
-      stagger: {
-        amount: 1
-      }
-    });
-  };
-
-  const fadeOut = element => {
-    gsap.to(element, 1, {
-      opacity: 0,
-      y: -20,
-      ease: "power4.out"
-    });
-  };
-
-  intersection && intersection.isIntersecting
-    ? fadeIn(".fadeIn")
-    : fadeOut(".fadeIn");
-
-
-    
-  return(
-    <div>
-      <ThemeProvider theme={theme}>
-        <Header/>
-        <HeaderBorder/>
-    
-          <Projects ref={sectionRef}>
-          <Container>
-          <Row>
-              <Col xl={6} lg={6} md={6}>
-                <a href="#" className="d-block mb-4 h-100">
-                  <Tile className="fadeIn">
-                      <img className="img-fluid" src={ProjectA}/>
-                  </Tile>
-                </a>
-              </Col>
-              <Col xl={6} lg={6} md={6}>
-                <a href="#" className="d-block mb-4 h-100">
-                  <Tile className="fadeIn">
-                      <img className="img-fluid" src={ProjectB}/>
-                  </Tile>
-                </a>
-              </Col> 
-            </Row>
-            <Row>
-              <Col xl={6} lg={6} md={6}>
-                <a href="#" className="d-block mb-4 h-100">
-                  <Tile className="fadeIn">
-                      <img className="img-fluid" src={ProjectC}/>
-                  </Tile> 
-                </a>
-              </Col> 
-              <Col xl={6} lg={6} md={6}>
-                <a href="#" className="d-block mb-4 h-100">
-                  <Tile className="fadeIn">
-                      <img className="img-fluid" src={ProjectD}/>
-                  </Tile>
-                </a>
-              </Col>
-            </Row>
-            </Container>
-          </Projects>
-        <Footer/>
-        <GlobalStyles />
-      </ThemeProvider>
-    </div>
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Switch>
+        <Route path="/work" exact>
+          <Work />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </ThemeProvider>
   )
 }
 
 export default App;
-
