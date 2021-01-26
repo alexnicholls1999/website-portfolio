11d32d8b75d46f941f8e1cf5fd8257c5736d0883
alexnicholls1999 <azini@live.co.uk>
2021-01-26 17:26:12 +0000

    start of v1.4

--- src/App.js
+++ src/App.js
@@ -1,106 +1,25 @@
-import React from 'react';
-import theme from "./config/theme.js";
-import GlobalStyles from "./config/GlobalStyles";
-import {ThemeProvider} from 'styled-components';
-import { Switch, Route, useLocation, Redirect } from 'react-router-dom';
-import '../src/App.css';
-
-
-// Custom Hooks
-import useAuth from "./services/firebase/useAuth";
-
-// Components 
-
-import Login from "./Views/Login";
-import Work from "./Views/Work";
-import About from "./Views/About";
-import Loader from "./Components/Loader";
-
-// Firebase 
-
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import firebaseConfig from "./config/firebase";
-
-let initAttemptedRoute = "/"
-
-function Protected({ authenticated, children, ...rest }) {
-  initAttemptedRoute = useLocation().pathname;
-
-  return (
-    <Route 
-      {...rest}
-      render={({ location }) =>
-        authenticated ? (
-          children
-        ) : (
-          <Redirect 
-            to={{
-              pathname: "/login",
-              state: { from: location }
-            }}
-          />
-        )
-      }
-    />
-  )
-}
-
-function RedirectToDash({ authenticated, children, ...rest}){
-  return (
-    <Route
-      {...rest}
-      render={({ location }) => 
-        !authenticated ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: initAttemptedRoute,
-              state: { from: location }
-            }}
-          />
-        )
-      }
-    />
-  )
-}
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
-
-  const { isAuthenticated, signInEmailUser, loading} = useAuth(firebase.auth);
-
-
-  if (loading) {
-    return  <Loader />;
-  } 
- 
-
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Switch>
-        <Protected authenticated={isAuthenticated} exact path="/">
-          <Work />
-        </Protected>
-        <RedirectToDash authenticated={isAuthenticated} path="/login">
-          <Login 
-            signInEmailUser={signInEmailUser}
-          />
-        </RedirectToDash>
-        <Protected authenticated={isAuthenticated} path="/about">
-          <About />
-        </Protected>
-      </Switch>
-    </ThemeProvider>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
