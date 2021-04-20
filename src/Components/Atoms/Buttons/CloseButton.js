e3a709a046ecab5989060ba0bf3263aac6094e09
alexnicholls1999 <azini@live.co.uk>
2021-04-20 13:02:58 +0100

    add close button to mobile menu

---
+++ src/Components/Atoms/Buttons/CloseButton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import CloseButtonIcon from '../../../assets/CloseCircle.svg';
+
+const StyledCloseButtonIcon = styled.img`
+    right: 10px;
+
+`;
+
+function CloseButton({onClick}) {
+    return (
+        <>
+            <StyledCloseButtonIcon src={CloseButtonIcon} />
+        </>
+
+    )
+}
+
+CloseButton.defaultProps = {
+    onClick: () => {}
+}
+
+CloseButton.propTypes = {
+    onClick: PropTypes.func.isRequired
+}
+
+export default CloseButton;
--- src/Components/Organisms/Menu.js
+++ src/Components/Organisms/Menu.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import CloseButton from '../Atoms/Buttons/CloseButton'
 import Scrollbar from '../Atoms/Scrollbar'
 import ButtonControl from '../Molecules/ButtonControl'
 
 function Menu() {
     return (
-        <>
-            <Scrollbar />
+        <> 
+            {/* <Scrollbar /> */}
+            <CloseButton />
             <br/>
             <h4 style={{fontWeight: "bold"}}>MENU</h4>
             <ButtonControl />
